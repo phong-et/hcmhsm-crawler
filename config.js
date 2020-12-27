05cb5cd3c9933ef463acd785a330730c5b2c5182
Phillip <phillip7.et@gmail.com>
2020-12-27 22:15:31 +0700

    add crawl module for all cities

--- config.js
+++ config.js
@@ -1,9 +1,24 @@
 module.exports = {
-  fetchUrls: {
-    hanoi: 'http://hanoiedu.vn/TraCuu/TraCuu',
-    hochiminh: 'http://diemthi.hcm.edu.vn/Home/Show',
-    tayninh:
-      'https://baotayninh.vn/ket-qua-diem-thi-thpt-tay-ninh.html?tensbd=&cumthi=&p=',
+  cites: {
+    hanoi:{
+      url:'http://hanoiedu.vn/TraCuu/TraCuu',
+      prefixId:'01',
+      cityId:1,
+      endId:79236,
+      crawlerGroup:'hanoi'
+    },
+    hochiminh:{
+      url:'http://diemthi.hcm.edu.vn/Home/Show',
+      prefixId:'02',
+      cityId:3,
+      endId:74669,
+    },
+    tayninh:{
+      url:'https://baotayninh.vn/ket-qua-diem-thi-thpt-tay-ninh.html?tensbd=&cumthi=&p=',
+      prefixId:'46',
+      cityId:46,
+      endId:8564,
+    },
   },
   dbUrl: 'mongodb://localhost:27017/mark',
 };
